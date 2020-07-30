598e8526ef169ca0d5978af075ba7f97a75b17e2
Victor Wiedemann <victor.wiedemann@gmail.com>
2020-07-30 12:05:20 -0700

    Creating my go online course project

---
+++ application_structure/main.go
@@ -0,0 +1,26 @@
+/*package main
+
+import "fmt"
+
+const secondsInHour = 3600
+
+func main() {
+	fmt.Println("Hello Go World!")
+	distance := 60.8
+	fmt.Printf("The distance in miles is %f \n", distance*0.62137)
+
+}*/
+
+package main
+
+import "fmt"
+
+func main() {
+	SayHello()
+}
+func SayHello() {
+	fmt.Println("Hello package v1.0.0!")
+
+	var x int = 10
+	_ = x
+}
